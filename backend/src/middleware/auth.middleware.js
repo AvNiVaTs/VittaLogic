d45af4d38ab1c7a0998299bd57fae2cc8d4e1d84
AvNiVaTs <avnivts@gmail.com>
2025-06-24 18:38:45 +0530

    added authentication middleware

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,28 @@
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiErr } from "../utils/ApiError.js"
+import jwt from "jsonwebtoken"
+import {Organization} from "../models/organization.model.js"
+
+export const verifyJWT = asyncHandler(async(req, res, next) => {
+    try{
+        const token = req.cookie?.accessToken || req.header("Authorized")?.replace("Bearer ", "")
+
+        if(!token){
+            throw new ApiErr(401, "Unauthorized Person")
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+        const org = await Organization.findById(decodedToken?._id).select("-authorizedPerson.password -refreshToken")
+
+        if(!org){
+            throw new ApiErr(401, "Invalid Access Token")
+        }
+
+        req.org = org;
+        next()
+    }
+    catch(err){
+        throw new ApiResponse(401, err?.message || "Invalid Access Token")
+    }
+})
